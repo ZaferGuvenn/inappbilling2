fef73c2ec735715d85d9f2240432b50838ca6690
zaferguvenn <zafer.guven58@gmail.com>
2023-05-22 05:41:58 +0300

    Initial commit

---
+++ app/src/main/java/com/lafimsize/inappbilling/MainActivity.kt
@@ -0,0 +1,179 @@
+package com.lafimsize.inappbilling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.billingclient.api.AcknowledgePurchaseParams
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClient.BillingResponseCode
+import com.android.billingclient.api.BillingClient.ProductType
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingFlowParams
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.ProductDetails
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.PurchasesResult
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.QueryProductDetailsParams
+import com.lafimsize.inappbilling.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var billingClient:BillingClient
+    private var isConnected=false
+
+    private lateinit var productDetailss:MutableList<ProductDetails>
+
+
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val pUL= PurchasesUpdatedListener { billingResult, purchases ->
+
+            //launch billing denildikten sonra...
+            if (billingResult.responseCode==BillingResponseCode.OK && purchases!=null){
+
+                for (i in purchases){
+                    handlePurchase(i)
+                }
+
+                println("pUL tetiklendi!")
+            }
+
+
+        }
+
+
+        billingClient=BillingClient.newBuilder(this).enablePendingPurchases()
+            .setListener(pUL).build()
+
+        startBillingClientConnection()
+
+        binding.show.setOnClickListener {
+
+            showProducts()
+        }
+
+        binding.button.setOnClickListener {
+
+            buy30Elmas()
+
+        }
+
+
+
+    }
+
+    //initialize
+    private fun startBillingClientConnection(){
+
+        billingClient.startConnection(object : BillingClientStateListener{
+            override fun onBillingServiceDisconnected() {
+
+                //try reconnect
+                isConnected=false
+                println("disconnected!!")
+
+            }
+
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+
+                if (billingResult.responseCode==BillingResponseCode.OK){
+
+                    //query purchase here
+                    isConnected=true
+                    println("connected!!")
+
+
+                }
+
+            }
+        })
+
+    }
+
+
+    private fun showProducts(){
+
+        val productList= listOf(
+            QueryProductDetailsParams.Product.newBuilder().setProductId("elmas_30")
+                .setProductType(ProductType.INAPP)
+                .build()
+        )
+
+        val queryProductDetailsParams=QueryProductDetailsParams.newBuilder()
+            .setProductList(productList)
+            .build()
+
+        billingClient.queryProductDetailsAsync(queryProductDetailsParams){ billingResult,productDetails->
+
+            if (billingResult.responseCode==BillingResponseCode.OK){
+
+                println(productDetails)
+                productDetailss=productDetails
+                binding.textView.text=productDetails[0].title
+                binding.button.text=productDetails[0].oneTimePurchaseOfferDetails?.formattedPrice
+            }
+
+        }
+
+
+    }
+
+
+    private fun buy30Elmas(){
+
+        val productDetailsParamsList=
+            listOf(BillingFlowParams.ProductDetailsParams.newBuilder()
+
+                .setProductDetails(productDetailss[0]).build()
+            )
+
+        val billingFlowParams=
+            BillingFlowParams.newBuilder()
+                .setProductDetailsParamsList(productDetailsParamsList)
+                .build()
+
+        val billingResult =
+            billingClient.launchBillingFlow(this@MainActivity,billingFlowParams)
+
+    }
+
+    private fun handlePurchase(purchase: Purchase){
+
+
+        if (purchase.purchaseState==Purchase.PurchaseState.PURCHASED){
+
+            if (!purchase.isAcknowledged){
+
+                val acknowledgePurchaseParams=
+                    AcknowledgePurchaseParams.newBuilder()
+                        .setPurchaseToken(purchase.purchaseToken)
+
+                CoroutineScope(Dispatchers.IO).launch {
+
+                    val acknowledgePurchaseResult=
+                        billingClient.acknowledgePurchase(acknowledgePurchaseParams.build()) {
+
+                            println("satın alma doğrulandı!")
+                            println(it)
+
+                        }
+
+
+                }
+
+
+            }
+
+
+        }
+
+    }
+}
